b6d10c279a2c06798c7bd372885baef6fea6af69
MateuszGajo <mateusz.gajo9@wp.pl>
2025-09-05 20:41:51 +0200

    create simple udp server

--- app/main.go
+++ app/main.go
@@ -2,11 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
-var _ = net.ListenUDP
-
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+
+	if err != nil {
+		log.Fatal(fmt.Printf("couldn't resolve addres, err: %v", err))
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+
+	defer conn.Close()
+
+	if err != nil {
+		log.Fatal(fmt.Printf("couldn't create connection, err: %v", err))
+	}
+
+	buffer := make([]byte, 2048)
+
+	for {
+		size, source, err := conn.ReadFromUDP(buffer)
+
+		if err != nil {
+			log.Fatal(fmt.Printf("couldn't read msg err: %v", err))
+			break
+		}
+
+		recivedData := buffer[:size]
+
+		fmt.Printf("recived data: %s, source: %s \n", recivedData, source)
+
+		response := []byte{}
+
+		_, err = conn.WriteToUDP(response, source)
+
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
+
 }
